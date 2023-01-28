57fae91fec7e7371a1a20cc1cbc5c92dd82e6437
Gongmeda <gongmeda@gmail.com>
2023-01-28 17:14:19 +0900

    init

---
+++ src/main/java/org/sparcs/hackathon/hteam/mozipserver/MozipServerApplication.java
@@ -0,0 +1,13 @@
+package org.sparcs.hackathon.hteam.mozipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozipServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MozipServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparcs/hackathon/hteam/mozipserver/MozipServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparcs.hackathon.hteam.mozipserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozipServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
