fdd7087a3a4f195bb36a889150aba999d5fb244a
Gongmeda <gongmeda@gmail.com>
2023-01-29 03:31:55 +0900

    fix: cors 오륲

--- src/main/java/org/sparcs/hackathon/hteam/mozipserver/MozipServerApplication.java
+++ src/main/java/org/sparcs/hackathon/hteam/mozipserver/MozipServerApplication.java
@@ -2,10 +2,11 @@ package org.sparcs.hackathon.hteam.mozipserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MozipServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/org/sparcs/hackathon/hteam/mozipserver/config/security/SecurityConfig.java
+++ src/main/java/org/sparcs/hackathon/hteam/mozipserver/config/security/SecurityConfig.java
@@ -2,39 +2,25 @@ package org.sparcs.hackathon.hteam.mozipserver.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .cors().and()
-            .csrf().disable()
-            .httpBasic().disable()
-            .authorizeHttpRequests((authz) -> authz.anyRequest().permitAll());
-        return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource configurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.addAllowedOrigin("*");
-        configuration.addAllowedHeader("*");
-        configuration.addAllowedMethod("*");
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                    .addMapping("/**")
+                    .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                    .allowedOriginPatterns("*");
+            }
+        };
     }
 
     @Bean
